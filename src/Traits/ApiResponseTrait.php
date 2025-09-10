ce90413e401467db72b09b8049c1daa2222eb943
Maxwell Durrant <74561948+MPSTR24@users.noreply.github.com>
2025-09-10 22:17:38 +0100

    feat: added meta, made protected
    Added meta into the payload which the user can customise. By default it contains the status code.
    Made methods protected to aid in inheritance.

--- src/Traits/ApiResponseTrait.php
+++ src/Traits/ApiResponseTrait.php
@@ -9,83 +9,90 @@ use Illuminate\Http\JsonResponse;
 // @phpstan-ignore trait.unused
 trait ApiResponseTrait
 {
-    private function response(?string $message = null, $data = null, int $statusCode = 200): JsonResponse
+    private function response(?string $message = null, mixed $data = null, int $statusCode = 200, array $meta = []): JsonResponse
     {
-        return response()->json([
+        if ($statusCode === 204) {
+            return response()->noContent();
+        }
+        
+        $payload = [
             'message' => $message,
             'data' => $data,
-        ], $statusCode);
+            'meta'    => array_merge(['status' => $statusCode], $meta),
+        ];
+        
+        return response()->json($payload, $statusCode);
     }
 
     // --------------------------------------------------HTTP 200 Codes--------------------------------------------------
 
-    private function apiSuccess($message = 'Success', $data = null): JsonResponse
+    protected function apiSuccess($message = 'Success', $data = null): JsonResponse
     {
         return $this->response($message, $data, 200);
     }
 
-    private function apiCreated($message = 'Created', $data = null): JsonResponse
+    protected function apiCreated($message = 'Created', $data = null): JsonResponse
     {
         return $this->response($message, $data, 201);
     }
 
-    private function apiAccepted($message = 'Accepted', $data = null): JsonResponse
+    protected function apiAccepted($message = 'Accepted', $data = null): JsonResponse
     {
         return $this->response($message, $data, 202);
     }
 
-    private function apiNoContent(): JsonResponse
+    protected function apiNoContent(): JsonResponse
     {
         return $this->response(null, null, 204);
     }
 
     // --------------------------------------------------HTTP 400 Codes--------------------------------------------------
 
-    private function apiBadRequest($message = 'Bad Request', $data = null): JsonResponse
+    protected function apiBadRequest($message = 'Bad Request', $data = null): JsonResponse
     {
         return $this->response($message, $data, 400);
     }
 
-    private function apiUnauthorized($message = 'Unauthorized', $data = null): JsonResponse
+    protected function apiUnauthorized($message = 'Unauthorized', $data = null): JsonResponse
     {
         return $this->response($message, $data, 401);
     }
 
-    private function apiForbidden($message = 'Forbidden', $data = null): JsonResponse
+    protected function apiForbidden($message = 'Forbidden', $data = null): JsonResponse
     {
         return $this->response($message, $data, 403);
     }
 
-    private function apiNotFound($message = 'Not Found', $data = null): JsonResponse
+    protected function apiNotFound($message = 'Not Found', $data = null): JsonResponse
     {
         return $this->response($message, $data, 404);
     }
 
-    private function apiTeapot($message = "I'm a teapot", $data = null): JsonResponse
+    protected function apiTeapot($message = "I'm a teapot", $data = null): JsonResponse
     {
         return $this->response($message, $data, 418);
     }
 
-    private function apiUnprocessable($message = 'Unprocessable Content', $data = null): JsonResponse
+    protected function apiUnprocessable($message = 'Unprocessable Content', $data = null): JsonResponse
     {
         return $this->response($message, $data, 422);
     }
 
-    private function apiTooManyRequests($message = 'Too many requests', $data = null): JsonResponse
+    protected function apiTooManyRequests($message = 'Too many requests', $data = null): JsonResponse
     {
         return $this->response($message, $data, 429);
     }
 
     // --------------------------------------------------HTTP 500 Codes--------------------------------------------------
 
-    private function apiInternalServerError($message = 'Server Error', $data = null): JsonResponse
+    protected function apiInternalServerError($message = 'Server Error', $data = null): JsonResponse
     {
         return $this->response($message, $data, 500);
     }
 
     // --------------------------------------------------HTTP 500 Codes--------------------------------------------------
 
-    private function apiCustom($message = null, $data = null, $statusCode = 200): JsonResponse
+    protected function apiCustom($message = null, $data = null, $statusCode = 200): JsonResponse
     {
         return $this->response($message, $data, $statusCode);
     }
